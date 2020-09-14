465a54e4d45ccf0e5251b87f281c1fb981c090c4
hiimchinh <chinhhoang1196@gmail.com>
2020-09-14 13:43:19 +0700

    refactored add prompt method

--- calculator.rb
+++ calculator.rb
@@ -1,11 +1,15 @@
-Kernel.puts('Welcome to Calculator!')
-Kernel.puts("What's the first number?")
+def prompt(message)
+  Kernel.puts("=> " + message)
+end
+
+prompt('Welcome to Calculator!')
+prompt("What's the first number?")
 number1 = Kernel.gets().chomp()
 
-Kernel.puts("What's the second number?")
+prompt("What's the second number?")
 number2 = Kernel.gets().chomp()
 
-Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
 operator = Kernel.gets().chomp()
 
 if operator == '1'
@@ -18,4 +22,4 @@ else
   result = number1.to_f() / number2.to_f()
 end
 
-Kernel.puts("The result is #{result}!")
+prompt("The result is #{result}!")
