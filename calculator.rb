591d6674c32d57bfa85517b9107f5f0f41ce254d
hiimchinh <chinhhoang1196@gmail.com>
2020-09-14 16:34:58 +0700

    validate 2 input number

--- calculator.rb
+++ calculator.rb
@@ -2,12 +2,34 @@ def prompt(message)
   Kernel.puts("=> " + message)
 end
 
-prompt('Welcome to Calculator!')
-prompt("What's the first number?")
-number1 = Kernel.gets().chomp()
+def valid_number?(num)
+  num.to_i() != 0
+end
 
-prompt("What's the second number?")
-number2 = Kernel.gets().chomp()
+prompt('Welcome to Calculator!')
+number1 = ''
+loop do
+  prompt("What's the first number?")
+  number1 = Kernel.gets().chomp()
+  if valid_number?(number1)
+    break
+  else
+    prompt("Hmm... that doesn't look like a valid number")
+  end
+end
+
+number2 = ''
+
+loop do
+  prompt("What's the second number?")
+  number2 = Kernel.gets().chomp()
+  if valid_number?(number2)
+    break
+  else
+    prompt("Hmm. that doesn't look like a valid number")
+  end
+end
+
 
 prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
 operator = Kernel.gets().chomp()
