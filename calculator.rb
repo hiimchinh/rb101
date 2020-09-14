de7bb5206bd61dea12c5eb39745e9f28541ea5e5
hiimchinh <chinhhoang1196@gmail.com>
2020-09-14 16:43:43 +0700

    add validate operator

--- calculator.rb
+++ calculator.rb
@@ -32,7 +32,15 @@ loop do
     end
   end
   prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
-  operator = Kernel.gets().chomp()
+  operator = ''
+  loop do
+    operator = Kernel.gets().chomp()
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Must choose 1, 2, 3 or 4.")
+    end
+  end
   result = case operator
             when '1'
               number1.to_i() + number2.to_i()
