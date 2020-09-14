b694d2ecd99909f523cfa201e9db954219f004bf
hiimchinh <chinhhoang1196@gmail.com>
2020-09-14 16:38:20 +0700

    add prompt ask to repeat the calculation

--- calculator.rb
+++ calculator.rb
@@ -8,40 +8,44 @@ end
 
 prompt('Welcome to Calculator!')
 number1 = ''
-loop do
-  prompt("What's the first number?")
-  number1 = Kernel.gets().chomp()
-  if valid_number?(number1)
-    break
-  else
-    prompt("Hmm... that doesn't look like a valid number")
-  end
-end
-
-number2 = ''
 
 loop do
-  prompt("What's the second number?")
-  number2 = Kernel.gets().chomp()
-  if valid_number?(number2)
-    break
-  else
-    prompt("Hmm. that doesn't look like a valid number")
+  loop do
+    prompt("What's the first number?")
+    number1 = Kernel.gets().chomp()
+    if valid_number?(number1)
+      break
+    else
+      prompt("Hmm... that doesn't look like a valid number")
+    end
   end
+  
+  number2 = ''
+  
+  loop do
+    prompt("What's the second number?")
+    number2 = Kernel.gets().chomp()
+    if valid_number?(number2)
+      break
+    else
+      prompt("Hmm. that doesn't look like a valid number")
+    end
+  end
+  prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+  operator = Kernel.gets().chomp()
+  result = case operator
+            when '1'
+              number1.to_i() + number2.to_i()
+            when '2'
+              number1.to_i() - number2.to_i()
+            when '3'
+              number1.to_i() * number2.to_i()
+            when '4'
+              number1.to_f() / number2.to_f()
+            end
+  prompt("The result is #{result}!")
+  prompt("Do you want to calculate again? (y to do it again)")
+  answer = Kernel.gets().chomp().downcase()
+  break unless answer == 'y'
 end
 
-
-prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
-operator = Kernel.gets().chomp()
-result = case operator
-          when '1'
-            number1.to_i() + number2.to_i()
-          when '2'
-            number1.to_i() - number2.to_i()
-          when '3'
-            number1.to_i() * number2.to_i()
-          when '4'
-            number1.to_f() / number2.to_f()
-end
-
-prompt("The result is #{result}!")
