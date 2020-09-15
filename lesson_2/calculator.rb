3bb6d022d3963983041df3949e8e09c3331b25c6
hiimchinh <chinhhoang1196@gmail.com>
2020-09-15 11:27:17 +0700

    add i18n

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,5 +1,30 @@
-def prompt(message)
-  Kernel.puts("=> " + message)
+I18N = {
+  en: {
+    adding: "Adding",
+    subtracting: "Subtracting",
+    multiplying: "Multiplying",
+    dividing: "Dividing",
+    welcome: "Welcome to Calculator! Enter your name:",
+    empty_name: "Make sure your name is valid.",
+    first_number_prompt: "What's the first number?",
+    second_number_prompt: "What's the second number?",
+    invalid_number: "Hmm... that doesn't look like a valid number",
+    validate_operator: "Must choose 1, 2, 3 or 4.",
+    again_prompt: "Do you want to calculate again? (y to do it again)",
+    result: "The result is",
+    calculating: "the two numbers...",
+    operator_prompt: <<-MSG
+What operation would you like to perform?
+  1) add
+  2) subtract
+  3) multiply
+  4) divide
+MSG
+  }
+}
+
+def prompt(msg)
+  Kernel.puts("=> " + msg)
 end
 
 def integer?(input)
@@ -18,26 +43,26 @@ def valid_number?(input)
   integer?(input) || float?(input)
 end
 
-def operation_to_message(message)
-  message = case message
+def operation_to_message(operation)
+  operation = case operation
             when '1'
-              "Adding"
+              I18N[:en][:adding]
             when '2'
-              "Substracting"
+              I18N[:en][:subtracting]
             when '3'
-              "Multiplying"
+              I18N[:en][:multiplying]
             when '4'
-              "Dividing"
+              I18N[:en][:dividing]
             end
-  message
+  operation
 end
 
-prompt 'Welcome to Calculator! Enter your name:'
+prompt I18N[:en][:welcome]
 name = ''
 loop do
   name = Kernel.gets.chomp
   if name.empty?
-    prompt "Make sure your name is valid."
+    prompt I18N[:en][:empty_name]
   else
     break
   end
@@ -49,42 +74,36 @@ number1 = ''
 
 loop do
   loop do
-    prompt "What's the first number?"
+    prompt I18N[:en][:first_number_prompt]
     number1 = Kernel.gets.chomp
-    if is_valid_number? number1
+    if valid_number? number1
       break
     else
-      prompt "Hmm... that doesn't look like a valid number"
+      prompt I18N[:en][:invalid_number]
     end
   end
   number2 = ''
   loop do
-    prompt "What's the second number?"
+    prompt I18N[:en][:second_number_prompt]
     number2 = Kernel.gets.chomp
-    if is_valid_number? number2
+    if valid_number? number2
       break
     else
-      prompt "Hmm. that doesn't look like a valid number"
+      prompt I18N[:en][:invalid_number]
     end
   end
-  operator_prompt = <<-MSG
-What operation would you like to perform?
-  1) add
-  2) subtract
-  3) multiply
-  4)divide
-  MSG
-  prompt operator_prompt
+
+  prompt I18N[:en][:operator_prompt]
   operator = ''
   loop do
     operator = Kernel.gets.chomp
     if %w(1 2 3 4).include? operator
       break
     else
-      prompt "Must choose 1, 2, 3 or 4."
+      prompt I18N[:en][:validate_operator]
     end
   end
-  prompt "#{operation_to_message(operator)} the two numbers..."
+  prompt "#{operation_to_message(operator)} #{I18N[:en][:calculating]}"
   result =  case operator
             when '1'
               number1.to_f + number2.to_f
@@ -95,8 +114,8 @@ What operation would you like to perform?
             when '4'
               number1.to_f / number2.to_f
             end
-  prompt "The result is #{result.round(2)}!"
-  prompt "Do you want to calculate again? (y to do it again)"
+  prompt "#{I18N[:en][:result]} #{result.round(2)}!"
+  prompt I18N[:en][:again_prompt]
   answer = Kernel.gets.chomp.downcase
   break unless answer == 'y'
 end
