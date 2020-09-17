9b87f3b0dd7bd1e4c708dfce6801dbdced08a363
hiimchinh <chinhhoang1196@gmail.com>
2020-09-17 10:14:03 +0700

    return float instead of integer

--- easy1/array_average.rb
+++ easy1/array_average.rb
@@ -1,9 +1,10 @@
 def average(array)
   sum = array.reduce(:+)
-  sum / array.size
+  result = sum.to_f / array.size
+  result.round(2)
 end
 
 #test cases
-puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
-puts average([1, 5, 87, 45, 8, 8]) == 25
-puts average([9, 47, 23, 95, 16, 52]) == 40
+puts average([1, 6]) == 3.5 # integer division: (1 + 6) / 2 -> 3
+puts average([1, 5, 87, 45, 8, 8]) == 25.67
+puts average([9, 47, 23, 95, 16, 52]) == 40.33
