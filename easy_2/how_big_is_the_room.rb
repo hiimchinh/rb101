6e876692754b9e0d4308ff30f11e826cf4c1946b
hiimchinh <chinhhoang1196@gmail.com>
2020-09-17 18:49:51 +0700

    add further exploration

--- easy_2/how_big_is_the_room.rb
+++ easy_2/how_big_is_the_room.rb
@@ -1,10 +1,12 @@
-SQMETERS_TO_SQFEET = 10.7639
+SQ_FEET_TO_SQ_INCHES = 144
+SQ_FEET_TO_SQ_CENTIMETERS = 929.03
 
-puts "Enter the length of the room in meters:"
+puts "Enter the length of the room in feet:"
 length = gets.chomp.to_f
-puts "Enter the width of the room in meters:"
+puts "Enter the width of the room in feet:"
 width = gets.chomp.to_f
 
-area_in_meters = (length * width).round(2)
-area_in_feet = (area_in_meters * SQMETERS_TO_SQFEET).round(2)
-puts "The area of the room is #{area_in_meters} (#{area_in_feet} square feet)."
+area_in_sq_feet = (length * width).round(2)
+area_in_sq_inches = (area_in_sq_feet * SQ_FEET_TO_SQ_INCHES).round(2)
+area_in_sq_centimeters = (area_in_sq_feet * SQ_FEET_TO_SQ_CENTIMETERS).round(2)
+puts "The area of the room is #{area_in_sq_feet} square feet (#{area_in_sq_inches} square inches and #{area_in_sq_centimeters} square centimeters)."
