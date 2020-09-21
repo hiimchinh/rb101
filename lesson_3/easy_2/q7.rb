d26746274a5b243a57c8a34dd1855ce14c09e588
hiimchinh <chinhhoang1196@gmail.com>
2020-09-21 09:45:39 +0700

    add rest of solution for lesson 3 easy 2

---
+++ lesson_3/easy_2/q7.rb
@@ -0,0 +1,6 @@
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+flintstones.concat(%w(Dino Hoppy))
+p flintstones
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+flintstones.push('Dino').push('Hoppy')
+p flintstones
