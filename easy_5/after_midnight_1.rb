4cbdf13863eaafb24005ee2b2dcec293c7e72a0e
hiimchinh <chinhhoang1196@gmail.com>
2020-09-26 16:33:37 +0700

    add another function using time

--- easy_5/after_midnight_1.rb
+++ easy_5/after_midnight_1.rb
@@ -1,10 +1,19 @@
 
 
 DAY_TO_MINUTES = 1440
+
 def time_of_day(number)
   number %= DAY_TO_MINUTES
   hours, minutes = number.divmod(60)
   format('%02d:%02d', hours, minutes)
+
+end
+
+def time_of_day_2(number)
+  now = Time.now
+  first_moment_today = Time.new(now.year, now.month, now.day)
+  midnight_day = first_moment_today
+  (midnight_day + number * 60).strftime('%A %I:%M')
 end
 
 #test cases
