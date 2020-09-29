08016c51255be227ad5bc020c7ae03f884f26c77
hiimchinh <chinhhoang1196@gmail.com>
2020-09-29 21:56:36 +0700

    puts index instead of value

--- lesson_4/additional_practice/problem_5.rb
+++ lesson_4/additional_practice/problem_5.rb
@@ -1,9 +1,2 @@
 flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
-result = ''
-flintstones.each do |name|
-    if name.start_with?('Be')
-        result = name
-        break
-    end
-end
-puts result
+puts flintstones.index { |name| name.start_with? 'Be' }
