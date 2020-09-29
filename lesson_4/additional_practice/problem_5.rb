4229dbe24e136b8dfd8ead0cfb76dec4aaff1b28
hiimchinh <chinhhoang1196@gmail.com>
2020-09-29 21:55:16 +0700

    add solution

--- lesson_4/additional_practice/problem_5.rb
+++ lesson_4/additional_practice/problem_5.rb
@@ -1,1 +1,9 @@
 flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+result = ''
+flintstones.each do |name|
+    if name.start_with?('Be')
+        result = name
+        break
+    end
+end
+puts result
