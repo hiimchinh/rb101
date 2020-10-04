6be6a50c4ddc5bbf2044fe70f55f5c94343dbb32
hiimchinh <chinhhoang1196@gmail.com>
2020-10-04 10:49:41 +0700

    add more readable solution

--- lesson_5/practice_problems/problem_16.rb
+++ lesson_5/practice_problems/problem_16.rb
@@ -1,12 +1,13 @@
 def generate_uuid
   uuid = ''
   hexa_list = '0123456789abcdef'
-  32.times do |index|
-    if [8, 13, 18, 23].include?(index)
-      uuid << '-'
-    else
-      uuid << hexa_list[rand(hexa_list.size)]
+  sections = [8, 4, 4, 4, 12]
+  sections.each_with_index do |section, index|
+    section.times do |num|
+      random_hex = hexa_list[rand(hexa_list.size)]
+      uuid << random_hex
     end
+    uuid << '-' if index < sections.count - 1
   end
   uuid
 end
