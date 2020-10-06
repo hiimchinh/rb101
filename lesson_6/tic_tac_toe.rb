94b1659f33a2aa307cf6315ad1195f1a6fc905a8
Chính Hoàng <chinhhoang1196@gmail.com>
2020-10-06 19:44:48 +0700

    add detect winnder func

--- lesson_6/tic_tac_toe.rb
+++ lesson_6/tic_tac_toe.rb
@@ -53,7 +53,25 @@ def board_full?(brd)
 end
 
 def someone_won?(brd)
-  false
+  !!detect_winner(brd)
+end
+
+def detect_winner(brd)
+  winning_lines = [[1,2,3], [4,5,6], [7,8,9]] +
+                  [[1,4,7], [2,5,8], [3,6,9]] +
+                  [[1,5,9], [3,5,7]]
+  winning_lines.each do |line|
+    if brd[line[0]] == PLAYER_MARKER &&
+        brd[line[1]] == PLAYER_MARKER &&
+        brd[line[2]] == PLAYER_MARKER
+        return 'Player'
+    elsif brd[line[0]] == COMPUTER_MARKER &&
+          brd[line[1]] == COMPUTER_MARKER &&
+          brd[line[2]] == COMPUTER_MARKER
+        return 'Computer'
+    end
+  end
+  nil
 end
 
 board = initialize_board
@@ -63,4 +81,8 @@ loop do
   computer_places_pieces!(board)
   display_board(board)
   break if someone_won?(board) || board_full?(board)
+end
+
+if someone_won?(board)
+  prompt "#{detect_winner(board)} won!"
 end
