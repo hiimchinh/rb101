97d797fb80a8bfb9265a61eb11520a825016beed
Chính Hoàng <chinhhoang1196@gmail.com>
2020-10-06 19:30:11 +0700

    add pieces of computers, check if full board

--- lesson_6/tic_tac_toe.rb
+++ lesson_6/tic_tac_toe.rb
@@ -8,6 +8,7 @@ PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
 
 def display_board(brd)
+  system 'clear'
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
   puts "     |     |"
@@ -31,6 +32,11 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
+def computer_places_pieces!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
 def player_places_piece!(brd)
   square = ''
   loop do
@@ -42,8 +48,19 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  false
+end
+
 board = initialize_board
 display_board(board)
-player_places_piece!(board)
-puts board.inspect
-display_board(board)
+loop do
+  player_places_piece!(board)
+  computer_places_pieces!(board)
+  display_board(board)
+  break if someone_won?(board) || board_full?(board)
+end
