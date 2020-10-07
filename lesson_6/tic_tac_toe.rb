13ce4ada491589724a1318a50c94481e7f76dc8f
Chính Hoàng <chinhhoang1196@gmail.com>
2020-10-07 10:07:33 +0700

    add start game func

--- lesson_6/tic_tac_toe.rb
+++ lesson_6/tic_tac_toe.rb
@@ -80,23 +80,26 @@ def joinor(arr, delimeter = ', ', last_delimeter = 'or')
   arr.join(delimeter)
 end
 
-
-loop do
-  board = initialize_board
+def start_game
+  brd = initialize_board
   loop do
-    display_board(board)
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
-    computer_places_pieces!(board)
-    break if someone_won?(board) || board_full?(board)
+    display_board(brd)
+    player_places_piece!(brd)
+    break if someone_won?(brd) || board_full?(brd)
+    computer_places_pieces!(brd)
+    break if someone_won?(brd) || board_full?(brd)
   end
-  display_board(board)
-
-  if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
+  display_board(brd)
+  if someone_won?(brd)
+    prompt "#{detect_winner(brd)} won!"
   else
     prompt "It's a tie"
   end
+  detect_winner(brd)
+end
+
+loop do
+  start_game
   prompt "Do you want to play again?(y or n)"
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
