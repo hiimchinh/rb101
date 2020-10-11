ce98e3d3aeede03d2efbe5a4943889db070c910f
hiimchinh <chinhhoang1196@gmail.com>
2020-10-11 23:19:01 +0700

    add extra way without using capitalize

--- easy_7/capitalize_words.rb
+++ easy_7/capitalize_words.rb
@@ -1,6 +1,18 @@
 def word_cap(string)
-  string.split.map(&:capitalize).join(' ')
+  word_arrs = string.split.map do |word|
+    capitalize_word = ''
+    word.chars.each_with_index do |char, index|
+      if index == 0
+        capitalize_word << char.upcase
+      else
+        capitalize_word << char.downcase
+      end
+    end
+    capitalize_word
+  end
+  word_arrs.join(' ')
 end
+
 
 #test cases
 puts word_cap('four score and seven') == 'Four Score And Seven'
