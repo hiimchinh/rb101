72511342ad4dc41a3090379bcb17a398d6648bcc
hiimchinh <chinhhoang1196@gmail.com>
2020-10-13 14:05:48 +0700

    add param determine check only alphabetic or not

--- easy_7/staggered_caps_2.rb
+++ easy_7/staggered_caps_2.rb
@@ -1,8 +1,8 @@
-def staggered_case(string)
+def staggered_case(string, check_only_alphabetic_char = true)
   result = ''
   need_upper = true
   string.chars.each do |char|
-    if char =~ /[A-Za-z]/
+    if !check_only_alphabetic_char || char =~ /[A-Za-z]/
       if need_upper
         result += char.upcase
       else
@@ -21,3 +21,4 @@ end
 puts staggered_case('I Love Launch School!') == 'I lOvE lAuNcH sChOoL!'
 puts staggered_case('ALL CAPS') == 'AlL cApS'
 puts staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 nUmBeRs'
+puts staggered_case('ignore 77 the 444 numbers', false) == 'IgNoRe 77 ThE 444 NuMbErS'
