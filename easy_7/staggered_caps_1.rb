e5383f45986e976cc87197e4d7e7465d75fe8fe4
hiimchinh <chinhhoang1196@gmail.com>
2020-10-13 13:53:24 +0700

    add param to do first case upcase or downcase

--- easy_7/staggered_caps_1.rb
+++ easy_7/staggered_caps_1.rb
@@ -1,25 +1,20 @@
-def staggered_case(string)
-  returned_string = ''
-  index = 0
-  while index < string.size
-    current_char = string[index]
-    if current_char =~ /[A-Za-z]/
-      if index.even?
-        returned_string << current_char.upcase
-      else
-        returned_string << current_char.downcase  
-      end
+def staggered_case(string, first_case_upper = true)
+  result = ''
+  string.chars.each do |char|
+    if first_case_upper
+      result += char.upcase
     else
-      returned_string << current_char
+      result += char.downcase
     end
-    index += 1
+    first_case_upper = !first_case_upper
   end
-  returned_string
+  result
 end
 
 
 # test cases
-puts staggered_case('I Love Launch School!')
 puts staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
 puts staggered_case('ALL_CAPS') == 'AlL_CaPs'
 puts staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
+
+puts staggered_case('ignore 77 the 444 numbers', false) == 'iGnOrE 77 tHe 444 nUmBeRs'
