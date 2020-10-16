3389342ea394f20feb59872b4841523162664617
hiimchinh <chinhhoang1196@gmail.com>
2020-10-16 10:55:26 +0700

    finish the game

--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -1,5 +1,5 @@
-def is_busted?(cards)
-  true || total_all_cards(cards) > 21
+def busted?(cards)
+  total_all_cards(cards) > 21
 end
 
 def total_all_cards(cards)
@@ -48,23 +48,77 @@ def deal_a_card(deck)
   pick_a_random_card(avaiable_cards)
 end
 
+def prompt_player
+  answer = nil
+  loop do
+    puts "Hit or stay?"
+    answer = gets.chomp.downcase
+    break if ['hit', 'stay'].include?(answer)
+    puts "Invalid answer. Try again."
+  end
+  answer
+end
+
+def who_win?(player_cards, dealer_cards)
+  player_sum = total_all_cards(player_cards)
+  dealer_sum = total_all_cards(dealer_cards)
+  if player_sum > dealer_sum
+    'player'
+  elsif player_sum < dealer_sum
+    'dealer'
+  else
+    'tie'
+  end
+end
+
+def display_winner(result, player_cards, dealer_cards)
+  player_sum = total_all_cards(player_cards)
+  dealer_sum = total_all_cards(dealer_cards)
+  case result
+  when 'player'
+    puts "Player win! Player: #{player_sum}, dealer: #{dealer_sum} "
+  when 'dealer'
+    puts "Dealer win! Player: #{player_sum}, dealer: #{dealer_sum} "
+  else
+    puts "It's a tie Player: #{player_sum}, dealer: #{dealer_sum} "
+  end
+end
+
+# start the game
+
 deck = init_deck
 player_cards = []
 dealer_cards = []
 2.times { |_| player_cards << deal_a_card(deck) }
 2.times { |_| dealer_cards << deal_a_card(deck) }
 
-puts "Dealer has #{dealer_cards[0]} and unknown card"
-puts "You have: #{player_cards.join(' and ')}"
 loop do
-  answer = nil
   loop do
-    puts "Hit or stay?"
-    answer = gets.chomp.downcase
-    puts "answer is: #{player_cards.inspect} "
-    break if ['hit', 'stay'].include?(answer)
-    puts "Invalid answer. Try again."
+    puts "Dealer has #{dealer_cards[0]} and unknown card"
+    puts "You have: #{player_cards.join(' and ')}. Total is: (#{total_all_cards(player_cards)})"
+    answer = prompt_player
+    break if answer == 'stay'
+    player_cards << deal_a_card(deck)
+    break if busted?(player_cards)
   end
-break if is_busted?(player_cards)
+  
+  if busted?(player_cards)
+    puts "You are busted!"
+    break
+  end
+
+  loop do
+    dealer_cards << deal_a_card(deck)
+    break if busted?(dealer_cards) || total_all_cards(dealer_cards) >= 17
+  end
+
+  if busted?(dealer_cards)
+    puts "You win. Dealer has busted!"
+    break
+  end
+  
+  result = who_win?(player_cards, dealer_cards)
+  display_winner(result, player_cards, dealer_cards)
+  break
 end
-puts "Player is busted "
+
