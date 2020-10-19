4e6ddbc3987bc05176d08ab7976da180bdf542c8
hiimchinh <chinhhoang1196@gmail.com>
2020-10-19 07:52:35 +0700

    add edge cases for negative input and zero

--- easy_9/counting_up.rb
+++ easy_9/counting_up.rb
@@ -1,10 +1,14 @@
 def sequence(num)
-  arr = []
-  num.downto(1) { |n| arr.unshift(n) }
-  arr
+  if num <= 0
+    (num..1).to_a
+  else
+    (1..num).to_a
+  end
 end
 
 #test cases
 puts sequence(5) == [1, 2, 3, 4, 5]
 puts sequence(3) == [1, 2, 3]
 puts sequence(1) == [1]
+puts sequence(-1) == [-1, 0, 1]
+puts sequence(0) == [0, 1]
