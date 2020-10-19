f4d951f1cc4968e9e607216799b6c708e61a07e7
hiimchinh <chinhhoang1196@gmail.com>
2020-10-19 08:42:00 +0700

    solved edge case points exceed 100

--- easy_9/grade_book.rb
+++ easy_9/grade_book.rb
@@ -1,6 +1,7 @@
 def get_grade(score1, score2, score3)
   average_grade = (score1 + score2 + score3) / 3
   case average_grade
+  when 101.. then 'A+'
   when 90..100 then 'A'
   when 80..89 then  'B'
   when 70..79 then  'C'
@@ -12,3 +13,5 @@ end
 # test cases
 puts get_grade(95, 90, 93) == "A"
 puts get_grade(50, 50, 95) == "D"
+puts get_grade(101, 100, 99) == 'A'
+puts get_grade(103, 101, 101) == 'A+'
